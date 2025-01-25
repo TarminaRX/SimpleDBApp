8bbe22fa3c13655c5a88530a5d8ef3fcd405b5ee
TarminaRX <tarminarobes@hotmail.com>
2025-01-25 12:48:20 +0800

    start gui.

---
+++ src/main/java/com/krlelirnz/simpledbapp/Login.java
@@ -0,0 +1,19 @@
+package com.krlelirnz.simpledbapp;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+public class Login {
+    private JFrame mFrame;
+
+    Login(){
+        mFrame = new JFrame();
+        mFrame.setVisible(true);
+        mFrame.setSize(650, 400);
+        mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mFrame.setResizable(false);
+    }
+}
+
