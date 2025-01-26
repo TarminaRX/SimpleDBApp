7c5e964e05357fa9f3ce7fd816ad2e59640e9f1d
Navidad12 <elijahgabriel7104@gmail.com>
2025-01-26 16:28:57 +0800

    LoginFrame done, no back-end

--- src/main/java/com/krlelirnz/simpledbapp/Login.java
+++ src/main/java/com/krlelirnz/simpledbapp/Login.java
@@ -6,17 +6,37 @@ import java.awt.event.*;
 import java.util.*;
 
 public class Login implements ActionListener {
-    JFrame mFrame; TextField t1;
+    JFrame mFrame; TextField t1, t2; JButton b1;
 
     Login(){
-        mFrame = new JFrame();
+        mFrame = new JFrame("User Login");
         mFrame.setVisible(true);
-        mFrame.setSize(650, 400);
+        mFrame.setSize(300, 180);
         mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         mFrame.setResizable(false);
 
         Panel mainP = new Panel();
+        Panel secPanel = new Panel(new GridLayout(2, 2));
+        //Username panel
+        Panel uName = new Panel(); Panel uText = new Panel();
+        uName.add(new Label("Username:")); 
+        uText.add(t1 = new TextField(10));
+        secPanel.add(uName); secPanel.add(uText);
 
+        //Password panel
+        Panel pName = new Panel(); Panel pText = new Panel();
+        pName.add(new Label("Password:"));
+        pText.add(t2 = new TextField(10));
+        secPanel.add(pName); secPanel.add(pText);
+
+        //Button
+        Panel bton = new Panel();
+        b1 = new JButton("Login");
+        bton.add(b1);
+        b1.addActionListener(this);
+
+        mainP.add(secPanel); mainP.add(bton);
+        mFrame.add(mainP);
     }
     public void actionPerformed(ActionEvent e){
 
