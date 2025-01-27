c9a928873c2866e45cf5febf3ad0b83455bc8b4d
Navidad12 <elijahgabriel7104@gmail.com>
2025-01-27 12:43:11 +0800

    Login Back-End done! Admin and Guest left

--- src/main/java/com/krlelirnz/simpledbapp/Login.java
+++ src/main/java/com/krlelirnz/simpledbapp/Login.java
@@ -3,36 +3,38 @@ package com.krlelirnz.simpledbapp;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.sql.ResultSet;
+import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.*;
 
 public class Login implements ActionListener {
-    JFrame mFrame; TextField t1, t2; JButton b1; JLabel messE;
-    private DatabaseConnection jdbc;
-    private int logCount;
+    JFrame mFrame; JTextField t1; JButton b1; JLabel messE; JPasswordField p1;
+    DatabaseConnection jdbc; Connection conn;
+    private int logCount; String uname; String passw; String urole;
 
     Login(){
+        jdbc = new DatabaseConnection("test-sql.db");
         mFrame = new JFrame("User Login");
         mFrame.setVisible(true);
         mFrame.setSize(300, 180);
         mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mFrame.setResizable(false);
+        mFrame.setResizable(true);
 
         Panel mainP = new Panel();
         Panel secPanel = new Panel(new GridLayout(2, 2));
 
+
         //Username panel
         Panel uName = new Panel(); Panel uText = new Panel();
         uName.add(new Label("Username:")); 
-        uText.add(t1 = new TextField(10));
+        uText.add(t1 = new JTextField(10));
         secPanel.add(uName); secPanel.add(uText);
+
 
         //Password panel
         Panel pName = new Panel(); Panel pText = new Panel();
         pName.add(new Label("Password:"));
-        pText.add(t2 = new TextField(10));
+        pText.add(p1 = new JPasswordField(10));
         secPanel.add(pName); secPanel.add(pText);
         
 
@@ -42,28 +44,31 @@ public class Login implements ActionListener {
         bton.add(b1);
         b1.addActionListener(this);
 
-        mainP.add(secPanel); mainP.add(bton); mainP.add(messE);
+        
+        //Error messsage
+        Panel error = new Panel();
+        messE = new JLabel("");
+        error.add(messE);
+
+        mainP.add(secPanel); mainP.add(bton); mainP.add(error);
         mFrame.add(mainP);
     }
+
     public void actionPerformed(ActionEvent e){
         String username = t1.getText();
-        String password = t2.getText();
-        jdbc = new DatabaseConnection("root", "root", "test-sql.db");
+        String password = new String(p1.getPassword());
         try {
+            logCount = 0;
             DatabaseConnection.User user = jdbc.getUser(username, password);
-            if(user != null){
-                logCount = 0;
-                openWindow(user);
-            } else{
-                logCount++;
+            openWindow(user);
+        } catch (SQLException e1) {
+            logCount++;
                 if(logCount >= 3){
                     JOptionPane.showMessageDialog(mFrame, "Too many failed login attempts");
                     System.exit(0);
+                }else{
+                    messE.setText("Invalid Username or Password. Attempts Left: " + (3 - logCount));
                 }
-                messE.setText("Invalid Username or Password. Attempts Left: " + logCount);
-            }
-        } catch (SQLException e1) {
-            messE.setText("Database connection failed" + e1.getMessage());
         }
     }
 
@@ -76,4 +81,3 @@ public class Login implements ActionListener {
         }
     }
 }
-
--- src/main/java/com/krlelirnz/simpledbapp/SimpleDBApp.java
+++ src/main/java/com/krlelirnz/simpledbapp/SimpleDBApp.java
@@ -4,8 +4,7 @@
 
 package com.krlelirnz.simpledbapp;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+
 
 /**
  *
@@ -14,18 +13,6 @@ import java.sql.SQLException;
 public class SimpleDBApp {
 
     public static void main(String[] args) {
-        // DatabaseConnection jdbc = new DatabaseConnection("root", "root", "test-sql.db");
-        // try {
-        //     ResultSet rs = jdbc.getAll();
-        //     while(rs.next()) {
-        //         System.out.println(rs.getString("username") + rs.getString("password"));
-        //     }
-        // } catch (SQLException e) {
-        //     System.err.println(e.toString());
-        //     System.err.println("");
-        // }
-        //  System.out.println("Working Directory = " + System.getProperty("user.dir"));
-        
-        Login log = new Login();
+        new Login();
         }
 }
